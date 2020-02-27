320567e9cae881d02574913fd163968fc9a767b6
cstaud <christian.staude@staffbase.com>
2020-02-27 16:22:08 +0100

    Initial self-updater

--- updateScript.sh
+++ updateScript.sh
@@ -1,31 +1,45 @@
-echo "Performing self-update..."
+#!/bin/bash
 
-echo -n "Downloading latest version..."
-if ! wget --quiet --output-document="$0.tmp" $UPDATE_BASE/$SELF ; then
-  echo "Failed: Error while trying to wget new version!"
-  echo "File requested: $UPDATE_BASE/$SELF"
-  exit 1
-fi
-echo "Done."
 
-# Copy over modes from old version
-OCTAL_MODE=$(stat -c '%a' $SELF)
-if ! chmod $OCTAL_MODE "$0.tmp" ; then
-  echo "Failed: Error while trying to set mode on $0.tmp."
-  exit 1
-fi
+#  echo -n "Inserting update process..."
+#  exec /bin/bash updateScript.sh
 
-# Spawn update script
-  cat > updateScript.sh << EOF
+SELF="$0"
+
+self_update() {
+  echo "Performing self-update ..."
+
+  echo -n "Downloading latest version..."
+  if ! wget --quiet --output-document="$SELF" ; then
+    echo "Failed: Error while trying to wget new version!"
+    echo "File requested: https://raw.githubusercontent.com/cstaud/bash_self-update/master/$SELF"
+    exit 1
+  fi
+  echo "Done."
+
+  # Copy over modes from old version
+#  OCTAL_MODE=$(stat -c '%a' "$SELF")
+#  if ! chmod "${OCTAL_MODE}" "$SELF.tmp" ; then
+#    echo "Failed: Error while trying to set mode on $0.tmp."
+#    exit 1
+#  fi
+#
+#   Spawn update script
+#  cat > updateScript.sh << EOF
 #!/bin/bash
 # Overwrite old file with new
-if mv "$0.tmp" "$0"; then
-  echo "Done. Update complete."
-  rm \$0
-else
-  echo "Failed!"
-fi
-EOF
+#if mv "$0.tmp" "$0"; then
+#  echo "Done. Update complete."
+#  rm \$0
+#else
+#  echo "Failed!"
+#fi
+#EOF
+}
 
-  echo -n "Inserting update process..."
-  exec /bin/bash updateScript.sh
+main() {
+   echo "Running v0"
+}
+
+self_update
+main
