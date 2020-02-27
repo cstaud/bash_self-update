37ce816e2362f24c67551116e8b80e0a3ba04fbe
cstaud <christian.staude@staffbase.com>
2020-02-27 16:27:38 +0100

    Testing self-update

--- updateScript.sh
+++ updateScript.sh
@@ -1,40 +1,20 @@
 #!/bin/bash
 
-
-#  echo -n "Inserting update process..."
-#  exec /bin/bash updateScript.sh
-
+REPO_URL="https://raw.githubusercontent.com/cstaud/bash_self-update/master"
 SELF="$0"
+REMOTE_SCRIPT="$REPO_URL/$SELF"
 
 self_update() {
   echo "Performing self-update ..."
 
-  echo -n "Downloading latest version..."
-  if ! wget --quiet --output-document="$SELF" ; then
+  echo "Downloading latest version..."
+  if ! wget --quiet --output-document="$SELF" "$REMOTE_SCRIPT"; then
     echo "Failed: Error while trying to wget new version!"
-    echo "File requested: https://raw.githubusercontent.com/cstaud/bash_self-update/master/$SELF"
+    echo "File requested: REMOTE_SCRIPT"
     exit 1
   fi
   echo "Done."
 
-  # Copy over modes from old version
-#  OCTAL_MODE=$(stat -c '%a' "$SELF")
-#  if ! chmod "${OCTAL_MODE}" "$SELF.tmp" ; then
-#    echo "Failed: Error while trying to set mode on $0.tmp."
-#    exit 1
-#  fi
-#
-#   Spawn update script
-#  cat > updateScript.sh << EOF
-#!/bin/bash
-# Overwrite old file with new
-#if mv "$0.tmp" "$0"; then
-#  echo "Done. Update complete."
-#  rm \$0
-#else
-#  echo "Failed!"
-#fi
-#EOF
 }
 
 main() {
