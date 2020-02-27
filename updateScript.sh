29f9ec9b502451d647b58c5fc831574febd81270
cstaud <christian.staude@staffbase.com>
2020-02-27 16:50:33 +0100

    Testing self-update

--- updateScript.sh
+++ updateScript.sh
@@ -5,19 +5,28 @@ SELF="$0"
 REMOTE_SCRIPT="$REPO_URL/$SELF"
 
 self_update() {
-  echo "Performing self-update ..."
 
-  echo "Downloading latest version..."
-  if ! wget --quiet --output-document="$SELF.new" "$REMOTE_SCRIPT"; then
-    echo "Failed: Error while trying to wget new version!"
-    echo "File requested: REMOTE_SCRIPT"
-    exit 1
+  echo "Checking for self-update ..."
+  diff=$(diff "$SELF" <(curl "$REMOTE_SCRIPT"))
+
+  if [ -z "$diff" ]
+  then
+    echo "Already running on latest version, yeah!"
+  else
+    echo "Downloading latest version..."
+
+    if ! wget --quiet --output-document="$SELF.new" "$REMOTE_SCRIPT"; then
+      echo "Failed: Error while trying to wget new version!"
+      echo "File requested: REMOTE_SCRIPT"
+      exit 1
+    fi
+    echo "Done. Quitting deprecated version now."
+    exit 0
   fi
-  echo "Done."
 }
 
 main() {
-   echo "Running v0"
+  echo "Running v0"
 }
 
 self_update
